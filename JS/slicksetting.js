12cc7de6ea53acdfd67d6a93207d1eb7663d88f8
cogeePL <koczanowski@gmail.com>
2023-01-19 14:45:26 +0100

    Add files via upload

---
+++ JS/slicksetting.js
@@ -0,0 +1,14 @@
+// $('.slick-carousel').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     centerMode: true,
+//     variableWidth: true
+//   });
+  $('.slick-carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
